1c1cff7541044fb47400513d69bd9a0bc2ec92c2
Rafael Chacin <rafaelchq@gmail.com>
2020-10-30 17:34:56 +0100

    Fixed Sass with Node 15

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <Router basename="/portfolioval.react/">
+    <Router basename="/portfolioval.react">
       <Header />
       <Switch>
         <Route exact path="/">
